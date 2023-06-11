3f771c0ab78b1ca5cc2082b588983d67f5411a64
Johann Dietrich <johann.dietrich@googlemail.com>
2023-06-11 13:58:12 +0200

    add functions to Vector2D

--- js/Vector2D.js
+++ js/Vector2D.js
@@ -3,4 +3,31 @@ export class Vector2D {
         this.x = x;
         this.y = y;
     }
+
+    /**
+     * 
+     * @param {Vector2D} vec 
+     */
+    add(vec) {
+        this.x += vec.x;
+        this.y += vec.y;
+    }
+
+    /**
+     * 
+     * @param {Vector2D} vec 
+     */
+    sub(vec) {
+        this.x -= vec.x;
+        this.y -= vec.y;
+    }
+
+    /**
+     * 
+     * @param {number} value 
+     */
+    scale(value) {
+        this.x *= value;
+        this.y *= value;
+    }
 }
