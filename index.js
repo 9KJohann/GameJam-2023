71e29fbbec256f0ead94663349576792b57c52f4
Johann Dietrich <johann.dietrich@googlemail.com>
2023-06-11 19:57:51 +0200

    update electron config

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
-    const win = new BrowserWindow({ width: 1280, height: 1024 });
+    const win = new BrowserWindow({ width: 1280, height: 1024, fullscreen: true, });
+    win.maximize();
 
     win.loadFile('index.html');
+    win.show();
+
+    win.addEventListener('keydown', (e) => {
+        if (e.key == "Escape") {
+            win.quit();
+        }
+    }, true)
+    return win;
 }
 
 app.whenReady().then(() => createWindow());
