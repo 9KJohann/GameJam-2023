e2869f92f9a4594e37db3f16a92836c9bf7d1cf0
Philipp <34624412+philihoffi@users.noreply.github.com>
2023-06-11 17:54:26 +0200

    add Start Screen

--- js/main.js
+++ js/main.js
@@ -19,13 +19,19 @@ function main() {
     const context = gameContext.renderingContext;
 
     let lost = false;
+    let start = false;
     //const textFont = new FontFace("Press Start 2P", "url(font_Press_Start_2P/PressStart2P-Regular.ttf)");
 
     window.addEventListener("loose", () => {
         lost = true;
     });
 
+    window.addEventListener("start", () => {
+        start = true;
+    });
+
     const background = new Entity("images/Level_1_Background.png");
+    const startScreen = new Entity("images/Start_Background.png");
     const terrain = new LevelTerrain("images/Level_1_Background_Collision.png")
 
     const bee = new MoveableEntity(
@@ -98,62 +104,68 @@ function main() {
     }
 
     function onDraw() {
-        onUpdate();
-        // canvas leeren
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        if (start) {
+            onUpdate();
+            // canvas leeren
+            context.clearRect(0, 0, canvas.width, canvas.height);
 
-        // mit weiß füllen, damit das canvas sichtbar ist
-        context.fillStyle = "white";
-        context.fillRect(0, 0, canvas.width, canvas.height);
+            // mit weiß füllen, damit das canvas sichtbar ist
+            context.fillStyle = "white";
+            context.fillRect(0, 0, canvas.width, canvas.height);
 
-        // zeichnen
-        background.width = canvas.width;
-        background.height = canvas.height;
-        //background.draw(context); // eigentlich sollte der Background so gezeichnet werden, allerdings wurde etwas in Entity geändert, wodurch der background nicht mehr richtig gezeichnet wird.
-        context.drawImage(background.image, 0, 0);
+            // zeichnen
+            background.width = canvas.width;
+            background.height = canvas.height;
+            //background.draw(context); // eigentlich sollte der Background so gezeichnet werden, allerdings wurde etwas in Entity geändert, wodurch der background nicht mehr richtig gezeichnet wird.
+            context.drawImage(background.image, 0, 0);
 
 
-        for (const entity of entities) {
-            entity.draw(context);
+            for (const entity of entities) {
+                entity.draw(context);
+            }
+
+            for (const collectable of collectables) {
+                collectable.draw(context);
+            }
+
+            // debug stuff
+            if (debug) {
+                let line = 0;
+
+                drawDebugText(context, `speedX=${bee.speedX}`, line++);
+                drawDebugText(context, `speedY=${bee.speedY}`, line++);
+                drawDebugText(context, `accelX=${bee.accelerationX}`, line++);
+                drawDebugText(context, `accelY=${bee.accelerationY}`, line++);
+
+                drawDebugText(context, `collisionX=${bee.collidesWith(ducky)}`, line++);
+
+                let debugStrInput = "";
+                if (input.upPressed) {
+                    debugStrInput += "UP ";
+                }
+                if (input.downPressed) {
+                    debugStrInput += "DOWN ";
+                }
+                if (input.leftPressed) {
+                    debugStrInput += "LEFT ";
+                }
+                if (input.rightPressed) {
+                    debugStrInput += "RIGHT ";
+                }
+                drawDebugText(context, debugStrInput, line++);
+
+                drawDebugText(context, "Bee is at: " + terrain.areaAtPixel(bee.x, bee.y), line++);
+                drawDebugText(context, "Color is: " + terrain.colorAtPixel(bee.x, bee.y), line++);
+                drawDebugText(context, `keyCollected=${key.isCollected()}`, line++);
+            }
+            if (lost) {
+                drawEndScreen();
+            }
+        } else {
+            drawStartScreen();
+
         }
 
-        for (const collectable of collectables) {
-            collectable.draw(context);
-        }
-
-        // debug stuff
-        if (debug) {
-            let line = 0;
-
-            drawDebugText(context, `speedX=${bee.speedX}`, line++);
-            drawDebugText(context, `speedY=${bee.speedY}`, line++);
-            drawDebugText(context, `accelX=${bee.accelerationX}`, line++);
-            drawDebugText(context, `accelY=${bee.accelerationY}`, line++);
-
-            drawDebugText(context, `collisionX=${bee.collidesWith(ducky)}`, line++);
-
-            let debugStrInput = "";
-            if (input.upPressed) {
-                debugStrInput += "UP ";
-            }
-            if (input.downPressed) {
-                debugStrInput += "DOWN ";
-            }
-            if (input.leftPressed) {
-                debugStrInput += "LEFT ";
-            }
-            if (input.rightPressed) {
-                debugStrInput += "RIGHT ";
-            }
-            drawDebugText(context, debugStrInput, line++);
-
-            drawDebugText(context, "Bee is at: " + terrain.areaAtPixel(bee.x, bee.y), line++);
-            drawDebugText(context, "Color is: " + terrain.colorAtPixel(bee.x, bee.y), line++);
-            drawDebugText(context, `keyCollected=${key.isCollected()}`, line++);
-        }
-        if (lost) {
-            drawEndScreen();
-        }
         requestAnimationFrame(onDraw);
     }
 
@@ -177,6 +189,28 @@ function main() {
         context.textAlign = "center";
         context.fillText("you lost", 1280 / 2, 1024 / 2);
 
+    }
+
+    function drawStartScreen() {
+        input.updateInput();
+        if (input.upPressed || input.downPressed || input.leftPressed || input.rightPressed) {
+            start = true;
+        }
+
+        context.drawImage(startScreen.image, 0, 0, 1280, 1024);
+        let width = 600;
+        let height = 200;
+        context.fillStyle = "#000000";
+        context.fillRect(1280 / 2 - width / 2, 1024 / 2 - height / 2, width, height);
+
+        context.fillStyle = "#FFD700";
+        context.fillRect(1280 / 2 - (width - 10) / 2, 1024 / 2 - (height - 10) / 2, (width - 10), (height - 10));
+
+        let fontsize = 100;
+        context.font = `${fontsize}px 'Press Start 2P'`;
+        context.fillStyle = "black";
+        context.textAlign = "center";
+        context.fillText("start", 1280 / 2, 1024 / 2 + fontsize / 2);
     }
 
 
